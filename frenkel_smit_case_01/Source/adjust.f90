766b46920bea85c0c1500654f4cc2efdbb2656d2
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-04-14 11:34:06 +0700

    FrenkelSmit case 01

---
+++ frenkel_smit_case_01/Source/adjust.f90
@@ -0,0 +1,36 @@
+SUBROUTINE ADJUST(Attemp, Nacc, Dr)
+!
+!     adjusts maximum displacement such that 50% of the
+!     movels will be accepted
+!
+!  Attemp (input) number of attemps that have been performed to displace a particle
+!  Nacc   (input) number of successful attemps to displace a particle
+!  Dr     (output) new maximum displacement
+!
+ 
+      IMPLICIT NONE
+      INCLUDE 'system.inc'
+      INTEGER :: Attemp, Nacc, attempp, naccp
+      REAL(8) :: dro, frac, Dr
+      SAVE naccp, attempp
+ 
+      IF (Attemp.EQ.0.OR.attempp.GE.Attemp) THEN
+         naccp = Nacc
+         attempp = Attemp
+      ELSE
+         frac = DBLE(Nacc-naccp)/DBLE(Attemp-attempp)
+         dro = Dr
+         Dr = Dr*ABS(frac/0.5D0)
+c        ---limit the change:
+         IF (Dr/dro.GT.1.5D0) Dr = dro*1.5D0
+         IF (Dr/dro.LT.0.5D0) Dr = dro*0.5D0
+         IF (Dr.GT.HBOX/2.D0) Dr = HBOX/2.D0
+         WRITE (6, 99001) Dr, dro, frac, Attemp - attempp, Nacc - naccp
+c        ---store nacc and attemp for next use
+         naccp = Nacc
+         attempp = Attemp
+      END IF
+      RETURN
+99001 FORMAT (' Max. displ. set to : ', f6.3, ' (old : ', f6.3, ')', /, 
+     &        ' Frac. acc.: ', f4.2, ' attempts: ', i7, ' succes: ', i7)
+      END
