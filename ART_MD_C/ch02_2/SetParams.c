61d5881a9738ff7972d3e907547bf11012a3cf3e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-08-24 21:13:53 +0700

    Start print out debug messages

--- ART_MD_C/ch02_2/SetParams.c
+++ ART_MD_C/ch02_2/SetParams.c
@@ -1,9 +1,29 @@
 void SetParams ()
 {
+
+  printf("---------------\n");
+  printf("Enter SetParams\n");
+  printf("---------------\n");
+
   rCut = pow (2., 1./6.);
-  VSCopy (region, 1. / sqrt (density), initUcell);
+  
+  VSCopy(region, 1. / sqrt (density), initUcell);
+  
   nMol = VProd (initUcell);
+  
   velMag = sqrt (NDIM * (1. - 1. / nMol) * temperature);
-  VSCopy (cells, 1. / (rCut + rNebrShell), region);
+  
+  VSCopy(cells, 1. / (rCut + rNebrShell), region);
+  printf("cells.x = %d\n", cells.x);
+  printf("cells.y = %d\n", cells.y);
+
   nebrTabMax = nebrTabFac * nMol;
+  printf("nebrTabMax = %d\n", nebrTabFac);
+
+
+  printf("---------------\n");
+  printf("Enter SetParams\n");
+  printf("---------------\n");
+
+
 }
--- ART_MD_C/ch02_2/SetupJob.c
+++ ART_MD_C/ch02_2/SetupJob.c
@@ -1,12 +1,12 @@
 void SetupJob ()
 {
-  AllocArrays ();
-  InitRand (randSeed);
+  AllocArrays();
+  InitRand(randSeed);
   stepCount = 0;
-  InitCoords ();
-  InitVels ();
-  InitAccels ();
-  AccumProps (0);
+  InitCoords();
+  InitVels();
+  InitAccels();
+  AccumProps(0);
   nebrNow = 1;
   countVel = 0;
 }
