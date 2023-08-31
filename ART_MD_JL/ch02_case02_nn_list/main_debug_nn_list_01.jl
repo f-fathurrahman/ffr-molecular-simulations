40e29f83e54bbb4ab139b5a830aa6064f4e2f1f4
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-08-31 19:26:11 +0700

    Debug linked list

--- ART_MD_JL/ch02_case02_nn_list/main_debug_nn_list_01.jl
+++ ART_MD_JL/ch02_case02_nn_list/main_debug_nn_list_01.jl
@@ -39,13 +39,77 @@ function v_cell_wrap!(
 end
 
 
+function build_cell_list!(cell_list, atoms, region, cells)
 
-#function main()
+    println()
+    println("Bulding cell_list:")
+    println()
+
+    invWid = cells ./ region
+
+    Natoms = atoms.Natoms
+
+    # indexing starts from 1, 0 is invalid value
+    fill!(cell_list, 0)
+    rs = zeros(Float64, 2)
+    cc = zeros(Int64, 2)
+
+    prev_c = 0
+    for ia in 1:Natoms
+        # shift the coordinates (?)
+        rs[1] = atoms.r[1,ia] + 0.5*region[1]
+        rs[2] = atoms.r[2,ia] + 0.5*region[2]
+        #
+        cc[1] = floor(Int64, rs[1] * invWid[1])
+        cc[2] = floor(Int64, rs[2] * invWid[2])
+        #
+        c = idx_vlinear(cc, cells) + Natoms + 1 # offset by 1
+        # c will always larger than Natoms
+        #println("\nia = ", ia, " rs = ", rs)
+        #println("ia = ", ia, " cc = ", cc, " c = ", c)
+        println("\nCurrent cell_list")
+        println("Atom index: ", ia, " linear index: ", c)
+        if prev_c != c
+            println("Different cell")
+        end
+        # What are these ?
+        cell_list[ia] = cell_list[c] # pointer to next data
+        cell_list[c] = ia # the data
+        print_cell_list(cell_list, Natoms)
+        prev_c = c
+    end
+    return
+end
+# The cell_list[Natoms+1:Natoms+Ncells] contain the first index of the list
+# cell_list[1:Natoms] contains index to next data
+
+
+function print_cell_list(cell_list, Natoms)
+    N = size(cell_list, 1)
+    for i in 1:N
+        @printf("%3d ", i)
+        if i == Natoms
+            print(" | ")
+        end
+    end
+    println()
+    for i in 1:N
+        @printf("%3d ", cell_list[i])
+        if i == Natoms
+            print(" | ")
+        end
+    end
+    println()
+end
+
+
+
+function main()
     Random.seed!(1234)
     # Use small initUcell
     sim = Simulation(
         InputVars(
-            Δt = 0.001, density = 0.8, initUcell = [4,4],
+            Δt = 0.001, density = 0.8, initUcell = [3,3],
             limit_vel = 4, range_vel = 3.0, size_hist_vel = 50,
             step_avg = 100, step_limit = 1000, step_vel = 5,
             temperature = 1.0,
@@ -68,44 +132,23 @@ end
 
     rr_nebr = (rCut + r_nebr_shell)^2
 
-    invWid = cells ./ region
-    println("region = ", region)
-    println("invWid = ", invWid)
+    build_cell_list!(cell_list, atoms, region, cells)
 
-    #fill!(cell_list, -1)
-    fill!(cell_list, 0) # indexing starts from 1, 0 is invalid value
-
-    rs = zeros(Float64, 2)
-    cc = zeros(Int64, 2)
-    for ia in 1:Natoms
-        # shift the coordinates (?)
-        rs[1] = atoms.r[1,ia] + 0.5*region[1]
-        rs[2] = atoms.r[2,ia] + 0.5*region[2]
-        # VMul(cc, rs, invWid);
-        cc[1] = floor(Int64, rs[1] * invWid[1])
-        cc[2] = floor(Int64, rs[2] * invWid[2])
-        c = idx_vlinear(cc, cells) + Natoms + 1 # offset by 1
-        # c will always larger than Natoms
-        println("\nia = ", ia, " rs = ", rs)
-        println("ia = ", ia, " cc = ", cc, " c = ", c)
-        #
-        cell_list[ia] = cell_list[c]
-        cell_list[c] = ia
-    end
-
-    println("Final cell_list:")
+    println("\nFinal cell_list:")
     for i in 1:length(cell_list)
         @printf("%3d %3d\n", i-1, cell_list[i]-1)
         # offset by 1 to get the same result as the original C
     end
 
     # Debug DO_CELL
+    # This is how we loop over cell and get the list of atoms
+    # within the cell
     for icell in (Natoms+1):length(cell_list)
         println("\nicell = ", icell - Natoms)
         j = cell_list[icell]
         while j > 0
-            println("Do something for j = ", j)
-            j = cell_list[j]
+            println("Atom index j = ", j)
+            j = cell_list[j] # next data
         end
     end
 
@@ -123,6 +166,7 @@ end
     for m1y in 0:cells[2]-1, m1x in 0:cells[1]-1
         m1v[1] = m1x
         m1v[2] = m1y
+        # get linear index of current cell (offset by Natoms+1)
         m1 = idx_vlinear(m1v, cells) + Natoms + 1
         @printf("\n[%d %d]: m1 = %d\n", m1x, m1y, m1)
         for ioff in 2:2
@@ -130,7 +174,7 @@ end
         end
     end
 
-#end
+end
 
 
 #=
@@ -167,4 +211,4 @@ function plot_debug(sim::Simulation)
 end
 =#
 
-#main()
+main()
