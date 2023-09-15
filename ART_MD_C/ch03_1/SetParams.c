869b6643ce772c6a32d12deb3e3e0832ac003ef0
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-09-15 21:38:57 +0700

    Print out calculated parameters

--- ART_MD_C/ch03_1/SetParams.c
+++ ART_MD_C/ch03_1/SetParams.c
@@ -1,8 +1,17 @@
 void SetParams ()
 {
   rCut = pow (2., 1./6.);
-  VSCopy (region, 1. / pow (density, 1./3.), initUcell);
-  nMol = VProd (initUcell);
-  velMag = sqrt (NDIM * (1. - 1. / nMol) * temperature);
-  VSCopy (cells, 1. / rCut, region);
+  printf("rCut = %f\n", rCut);
+
+  VSCopy(region, 1. / pow (density, 1./3.), initUcell);
+  printf("region = [%f,%f,%f]\n", region.x, region.y, region.z);
+  
+  nMol = VProd(initUcell);
+  printf("nMol = %d\n", nMol);
+
+  velMag = sqrt(NDIM * (1. - 1. / nMol) * temperature);
+  printf("velMag = %f\n", velMag);
+
+  VSCopy(cells, 1. / rCut, region);
+  printf("cells = [%d,%d,%d]\n", cells.x, cells.y, cells.x);
 }
